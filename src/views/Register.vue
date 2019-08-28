fb70e2017d5437dd5489ec9267120d2612e66c22
Aaron Hedges <aaron.hedges@gmail.com>
2019-08-28 14:16:55 +0200

    register form - update tabs for linter and add start of form validation

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="registration">
     <h1>New User Registration</h1>
+
+
     <div class="formHolder">
-        <div class="registrationForm">
-            <form>
-                <div class="row">
-                    <label>First Name: </label>
-                    <input type="text" name="firstName">
-                </div>
-                <div class="row">
-                    <label>Last Name: </label>
-                    <input type="text" name="lastName">
-                </div>
-                <div class="row">
-                    <label>Email: </label>
-                    <input type="text" name="email">
-                </div>
-                <div class="row">
-                    <label>Password: </label>
-                    <input type="text" name="password">
-                </div>
-                <div class="row">
-                    <label>Confirm Password: </label>
-                    <input type="text" name="passwordConfirm">
-                </div>
-                <div class="row">
-                    <input type="submit" value="Submit">
-                    <input type="reset" value="Clear Form" >
-                </div>
-            </form>
+      <div class="registrationForm">
+        <form
+          id="app"
+          @submit="checkForm"
+          action="registerSubmit"
+          method="post"
+        >
+          <div class="row" v-if="errors.length">
+            <p>
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+          </div>
+          <div class="row">
+            <label for="firstName">First Name: </label>
+            <input
+              id="firstName"
+              v-model="firstName"
+              name="firstName"
+              type="text"
+            >
+          </div>
+          <div class="row">
+            <label for="lastName">Last Name: </label>
+            <input
+              id="lastName"
+              v-model="lastName"
+              name="lastName"
+              type="text"
+            >
+          </div>
+          <div class="row">
+            <label for="email">Email: </label>
+            <input
+              id="email"
+              v-model="email"
+              name="email"
+              type="text"
+            >
+          </div>
+          <div class="row">
+            <label for="password">Password: </label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+            >
+          </div>
+          <div class="row">
+            <label for="passwordConfirm">Confirm Password: </label>
+            <input
+              id="passwordConfirm"
+              v-model="passwordConfirm"
+              name="passwordConfirm"
+              type="password"
+            >
+          </div>
+          <div class="row">
+            <input type="submit" value="Submit">
+            <input type="reset" value="Clear Form" >
+          </div>
+      </form>
         </div>
     </div>
   </div>
