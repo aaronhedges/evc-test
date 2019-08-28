a9a91f8e6d9b49a52f95162f018ea025f857ca57
Aaron Hedges <aaron.hedges@gmail.com>
2019-08-28 13:07:19 +0200

    new user registration - add basic form

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="registration">
+    <h1>New User Registration</h1>
+    <div class="formHolder">
+        <div class="registrationForm">
+            <form>
+                <div class="row">
+                    <label>First Name: </label>
+                    <input type="text" name="firstName">
+                </div>
+                <div class="row">
+                    <label>Last Name: </label>
+                    <input type="text" name="lastName">
+                </div>
+                <div class="row">
+                    <label>Email: </label>
+                    <input type="text" name="email">
+                </div>
+                <div class="row">
+                    <label>Password: </label>
+                    <input type="text" name="password">
+                </div>
+                <div class="row">
+                    <label>Confirm Password: </label>
+                    <input type="text" name="passwordConfirm">
+                </div>
+                <div class="row">
+                    <input type="submit" value="Submit">
+                    <input type="reset" value="Clear Form" >
+                </div>
+            </form>
+        </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.formHolder {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.registrationForm {
+    text-align: left;
+}
+.row {
+    margin: 10px;
+}
+</style>
