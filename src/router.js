08410f4cc93129547218408f55240801c02d2709
Aaron Hedges <aaron.hedges@gmail.com>
2019-08-29 15:12:31 +0200

    added routing for listing page

--- src/router.js
+++ src/router.js
@@ -17,7 +17,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Register.vue'),
+      component: () => import(/* webpackChunkName: "register" */ './views/Register.vue'),
+    },
+        {
+      path: '/listings',
+      name: 'listings',
+      component: () => import(/* webpackChunkName: "listings" */ './views/Listings.vue'),
     },
   ],
 });
